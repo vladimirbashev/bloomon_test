5584dc8cdca58c1d6955c3ec541859a34a886947
Vladimir Bashev <vladimirbashev@gmail.com>
2020-05-04 07:39:18 +0300

    Comments

--- parser.py
+++ parser.py
@@ -7,38 +7,49 @@ FLOWER_REGEX = r'(\d+)([a-z])'
 
 
 class Flower(ABC):
+    """An abstract class for a flower"""
+
     def __init__(self, specie, design_quantity=0, quantity=0):
         self.specie = specie
         self.weight = 0
-        self.design_quantity = int(design_quantity)
-        self.quantity = int(quantity)
+        self.design_quantity = int(design_quantity)  # number of a flower in bouquet design
+        self.quantity = int(quantity)  # actual number of flowers in bouquet
 
     @property
     def design(self):
+        """Check if a flower is a part of design"""
         return self.design_quantity > 0
 
     @property
     def required_quantity(self):
+        """Checked how many extra flowers needs to be added to complete a bouquet"""
         result = self.design_quantity - self.quantity
         return result if result > 0 else 0
 
     def __str__(self):
+        """Represents string value of a flower"""
         return str.format('{}{}', self.quantity, self.specie)
 
 
 class BouquetFlower(Flower):
+    """Created a flower. A bouquet flower means design_quantity>0"""
+
     def __init__(self, design_quantity, specie):
         super(BouquetFlower, self).__init__(specie, design_quantity=design_quantity)
 
 
 class ExtraFlower(Flower):
+    """Created a flower. An extra flower means design_quantity=0"""
+
     def __init__(self, quantity, specie):
         super(ExtraFlower, self).__init__(specie, quantity=quantity)
 
 
 class Bouquet:
+    """Creates a bouquet design, also used to construct bouquets"""
+
     def __init__(self, val):
-        self.active = True
+        self.active = True  # bouquet is deactivated if cannot be completed
         self.bouquet_design = val
         groups = re.findall(BOUQUET_REGEX, val)
         if groups:
@@ -48,17 +59,21 @@ class Bouquet:
 
     @property
     def completed(self):
+        """Check if a bouquet is completed"""
         return self.design_completed and sum(f.quantity for f in self.flowers) == self.total
 
     @property
     def design_completed(self):
+        """Check if a bouquet has all needed design flowers"""
         return all(f.design_quantity <= f.quantity for f in self.flowers)
 
     @property
     def extra_flowers_quantity(self):
+        """Checks how many extra flowers need to be added to complete a bouquet"""
         return self.total - sum(f.design_quantity for f in self.flowers)
 
     def add_extra_flower(self, quantity, specie):
+        """Adds an extra flowers to a bouquet"""
         fl = next((fl for fl in self.flowers if fl.specie == specie), None)
         if fl:
             fl.quantity = fl.quantity + quantity
@@ -66,6 +81,7 @@ class Bouquet:
             self.flowers.append(ExtraFlower(quantity, specie))
 
     def __str__(self):
+        """Represents string value of bouquet"""
         return str.format('{}{}{}',
                           self.name,
                           self.size,
@@ -73,19 +89,23 @@ class Bouquet:
 
 
 class Parser:
+    """Class implements logic for bouquets construction."""
+
     def __init__(self):
-        self.flowers = dict()
-        self.bouquets = list()
-        self.total_flowers = {
-                'L': 0,
-                'S': 0
-            }
+        self.flowers = dict()  # available flowers
+        self.bouquets = list()  # bouquet designs
+        self.total_flowers = {  # total available flowers, user to calculates weights
+            'L': 0,
+            'S': 0
+        }
 
     @staticmethod
     def _get_weight(total, quantity):
+        """Calculates weight of a flower"""
         return 1 - (total - quantity) / total
 
     def _fill_test_data(self):
+        """Prepares test data"""
         self._parse_bouquet_design('AL10a15b5c30')
         self._parse_bouquet_design('AS10a10b25')
         self._parse_bouquet_design('BL15b1c21')
@@ -101,9 +121,11 @@ class Parser:
             self._parse_flower('cS')
 
     def _parse_bouquet_design(self, line):
+        """Serialize string value into a bouquet object"""
         self.bouquets.append(Bouquet(line))
 
     def _parse_flower(self, line):
+        """Serialize string value into a flower object"""
         if line[0] not in self.flowers.keys():
             self.flowers[line[0]] = {
                 'L': 0,
@@ -113,6 +135,8 @@ class Parser:
         self.total_flowers[line[1]] += 1
 
     def parse(self, is_test):
+        """Method implements data input or test data usage(based on input parameter).
+        Data is stored on self.flowers and self.bouquets structures."""
         if is_test:
             self._fill_test_data()
         else:
@@ -133,12 +157,15 @@ class Parser:
                     break
 
     def print(self):
+        """Prints completed bouquets"""
         print('\nResult:')
         for bd in self.bouquets:
             if bd.completed:
                 print(str(bd))
 
     def sort_bouquets(self):
+        """Method sorts bouquet designs based on bouquets weight.
+        Weights reflects bouquet complexity, in other words big bouquets with rare flowers should be prepared first."""
         for bd in self.bouquets:
             if bd.total > self.total_flowers[bd.size]:
                 bd.active = False
@@ -157,6 +184,9 @@ class Parser:
         self.bouquets.sort(key=lambda b: b.weight, reverse=True)
 
     def construct_bouquets(self):
+        """Method constructs bouquets based on designs.
+        It is done in infinite loop. A one uncompleted bouquet is excluded from logic on each loop
+        to free flowers for other uncompleted bouquets"""
         while True:
             self._fill_required_flowers()
             self._fill_extra_flowers()
@@ -168,6 +198,7 @@ class Parser:
                 b.active = False
 
     def _fill_required_flowers(self):
+        """Adds design flowers to bouquets"""
         for bd in self.bouquets:
             if not bd.active or bd.completed or bd.design_completed:
                 continue
@@ -177,6 +208,7 @@ class Parser:
                     break
 
     def _fill_extra_flowers(self):
+        """Adds non design flowers to bouquets"""
         for bd in self.bouquets:
             if bd.completed or not bd.active or not bd.design_completed:
                 continue
@@ -192,6 +224,7 @@ class Parser:
                     break
 
     def _add_extra_flower(self, bouquet, specie):
+        """Add a non design flower to a bouquet"""
         quantity = self._get_flowers_quantity(specie, bouquet.size)
         if quantity <= 0:
             return False
@@ -202,6 +235,7 @@ class Parser:
         return True
 
     def _add_required_flower(self, flower, bouquet):
+        """Add a design flowers to a bouquet"""
         if flower.required_quantity == 0:
             return True
         elif flower.required_quantity <= self._get_flowers_quantity(flower.specie, bouquet.size):
@@ -213,12 +247,14 @@ class Parser:
             return False
 
     def _unreserve_flowers(self, bouquet):
+        """Removes flowers from a bouquet and adds flowers to available flowers"""
         for fl in bouquet.flowers:
             if fl.quantity:
                 self.flowers[fl.specie][bouquet.size] = self.flowers[fl.specie][bouquet.size] + fl.quantity
         bouquet.flowers = [fl for fl in bouquet.flowers if fl.quantity > 0 or fl.design]
 
     def _get_flowers_quantity(self, specie, size):
+        """Returns available number of flowers with provided specie and size"""
         if specie in self.flowers.keys() and size in self.flowers[specie].keys():
             return self.flowers[specie][size]
         else:
